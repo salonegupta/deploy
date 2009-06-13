453df152de45f076e9ad6bfefbb5ce1aaacaa2f9
Sean Ahn <sean@Sean-KUbuntu.(none)>
2009-06-12 18:09:03 -0700

    Patched the cron scheduler to work in cluster; SHOAL

---
+++ impl/src/main/java/org/intalio/deploy/deployment/impl/clustering/NullClusterListener.java
@@ -0,0 +1,17 @@
+package org.intalio.deploy.deployment.impl.clustering;
+
+import org.intalio.deploy.deployment.DeployedAssembly;
+
+public class NullClusterListener implements ClusterListener {
+	public void onActivated(DeployedAssembly assembly) {
+	}
+
+	public void onDeployed(DeployedAssembly assembly, boolean activate) {
+	}
+
+	public void onRetired(DeployedAssembly assembly) {
+	}
+
+	public void onUndeployed(DeployedAssembly assembly) {
+	}
+};
