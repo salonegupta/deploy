932a046536a2316df9caeafa8f0896a7936c3fb0
Sean Ahn <sean@Sean-KUbuntu.(none)>
2009-07-23 14:12:29 -0700

    serverId defaults to class 4 uuid

---
+++ impl/src/test/java/org/intalio/deploy/deployment/impl/clustering/QuorumBasedClusterTest.java
@@ -0,0 +1,19 @@
+package org.intalio.deploy.deployment.impl.clustering;
+
+import junit.framework.TestCase;
+
+public class QuorumBasedClusterTest extends TestCase {
+    public void testResolveServerId() throws Exception {
+        QuorumBasedCluster cluster1 = new QuorumBasedCluster();
+        String serverId1 = cluster1.getServerId();
+        assertNotNull(serverId1);
+        cluster1.start();
+        assertEquals(serverId1, cluster1.getServerId());
+        
+        QuorumBasedCluster cluster2 = new QuorumBasedCluster();
+        assertNotSame(cluster1.getServerId(), cluster2.getServerId());
+        System.setProperty(QuorumBasedCluster.DEPLOY_SERVER_ID_PROP, "junit2");
+        cluster2.start();
+        assertEquals("junit2", cluster2.getServerId());
+    }
+}
