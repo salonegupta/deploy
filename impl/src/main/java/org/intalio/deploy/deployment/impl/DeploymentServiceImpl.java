8b2d2443d875f3ae75779e5e09197a51f5f8f1e5
Veresh Jain <veresh@intalio.com>
2013-09-20 12:05:30 +0530

    Rollbacked last changes from Himanshu

--- impl/src/main/java/org/intalio/deploy/deployment/impl/DeploymentServiceImpl.java
+++ impl/src/main/java/org/intalio/deploy/deployment/impl/DeploymentServiceImpl.java
@@ -147,8 +147,6 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
     };
 
     private final ScanTask _scanTask = new ScanTask();
-    private final CheckNodeHealthTask _checkTask = new CheckNodeHealthTask();
-    private final ExecutorService _executorService = Executors.newFixedThreadPool(1);
 
     private DataSource _dataSource;
 
@@ -1136,9 +1134,8 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
 
                 _timer.schedule(_scanTask, _scanPeriod * 1000,
                         _scanPeriod * 1000);
-
-                _timer.schedule(
-                        _checkTask, _scanPeriod * 1000,
+                new Timer("Check Node health Timer", true).schedule(
+                        new CheckNodeHealthTask(), _scanPeriod * 1000,
                         _scanPeriod * 1000);
             }
         } finally {
@@ -1399,6 +1396,7 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
         }
     }
 
+    private final ExecutorService executorService = Executors.newFixedThreadPool(1);
     /**
      * Recurring scan of the deployment directory every "scanPeriod"
      * milliseconds
@@ -1423,7 +1421,7 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
                 }
             };
 
-            Future<Boolean> future = _executorService.submit(checkNodeHealth);
+            Future<Boolean> future = executorService.submit(checkNodeHealth);
             try {
                 Boolean isNodeHealthy = future.get(2, TimeUnit.SECONDS);
                 if (LOG.isDebugEnabled())
