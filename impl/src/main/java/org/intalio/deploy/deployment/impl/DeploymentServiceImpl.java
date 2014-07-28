892034f42c52c057422e0ecc8126ef254a5ff054
Himanshu Mishra <mishra@intalio.com>
2014-07-28 14:31:38 +0530

    BPMS-1189: Added validation for deployed zip

--- impl/src/main/java/org/intalio/deploy/deployment/impl/DeploymentServiceImpl.java
+++ impl/src/main/java/org/intalio/deploy/deployment/impl/DeploymentServiceImpl.java
@@ -412,7 +412,7 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
                 DeploymentResult result = null;
                 try {
                     boolean zipFound = Utils.unzip(zip, assemblyDir);
-
+                    zipFound = zipFound && verifyExplodedAssembly(assemblyDir);
                     if(!zipFound) {
                         DeploymentMessage deploymentMessage = new DeploymentMessage(Level.ERROR,
                                 "Provided file is not a valid archive file");
@@ -442,6 +442,25 @@ public class DeploymentServiceImpl implements DeploymentService, Remote, Cluster
             writeUnlockDeploy();
         }
 
+    }
+
+    /**
+     * This method checks whether exploded assembly contains ode component in
+     * process or not.
+     * 
+     * @param assemblyDir
+     * @return
+     */
+    private boolean verifyExplodedAssembly(File assemblyDir) {
+        if (assemblyDir != null) {
+            if (assemblyDir.exists()) {
+                for (String file : assemblyDir.list()) {
+                    if (file.endsWith(ODE_EXTENSION))
+                        return true;
+                }
+            }
+        }
+        return false;
     }
 
     //
